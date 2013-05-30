6bb88b678c049298d232b7c2ee2ac3b5eeb1b565
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-05-30 16:36:27 -0400

    move score.dart from sound to lib

--- lib/collision_clones.dart
+++ lib/collision_clones.dart
@@ -9,8 +9,8 @@ import 'package:simple_audio/simple_audio.dart';
 
 part 'cars.dart';
 part 'game_board.dart';
+part 'score.dart';
 part 'sound/audio.dart';
-part 'sound/score.dart';
 part 'util/color.dart';
 part 'util/git_commands.dart';
 part 'util/random.dart';
